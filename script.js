82b0a229300b205dfb87f66eaf2de7b7b1096a5f
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-10-02 22:19:03 -0300

    add try/catch(e)

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const URL_MAIN =
   "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
 
 async function getData() {
-  const response = await fetch(URL_MAIN);
-  const candidatos = await response.json();
-  console.log(candidatos.cand);
+  try {
+    const response = await fetch(URL_MAIN);
+    const candidatos = await response.json();
 
-  const candidatosBrasileiros = candidatos.cand.map((cand) => {
-    return `
+    const candidatosBrasileiros = candidatos.cand.map((cand) => {
+      return `
         <div id="wrap">
         <h2>${cand.nm}</h2>
        <p>${cand.pvap}%</p>
   </div>
     `;
-  });
-  container.innerHTML = candidatosBrasileiros.join("");
+    });
+    container.innerHTML = candidatosBrasileiros.join("");
+  } catch (e) {
+    console.log(e);
+  }
 }
 getData();
